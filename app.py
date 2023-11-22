e7fb4f1503d64e7c4f55add190033b27643639a8
roncalloj <107959215+roncalloj@users.noreply.github.com>
2023-11-22 17:11:14 +0000

    Rock, paper, scissors game with python

--- app.py
+++ app.py
@@ -4,8 +4,44 @@
 #-----------------------------------------------------------------------------------------
 
 from flask import Flask
+import random
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+play_options = ["Rock", "Paper", "Scissors"]
+computer = random.choice(play_options)
+player = False
+
+def re_start(option):
+    re_start = input(option).lower()
+    if re_start in ("y", "yes"):
+        print("Let's play again!")
+        return False
+    else:
+        print("Thanks for playing!")
+        return exit()
+
+def determine_winner(player, computer):
+    if player == computer:
+        print("Tie!")
+    elif (player == "Rock" and computer == "Scissors") or (player == "Paper" and computer == "Rock") or (player == "Scissors" and computer == "Paper"):
+        print("You win!", player, "beats", computer)
+    else:
+        print("You lose!", computer, "beats", player)
+
+while player == False:
+    print("Choose your weapon!")
+    player = input("Rock, Paper, Scissors? ").capitalize()
+
+    if player in play_options:
+        print("Player chose:", player)
+        print("Computer chose:", computer)
+        determine_winner(player, computer)
+        player = re_start("Would you like to play again? (y/n) ")
+    else:
+        print("Not a valid option.")
+        player = re_start("Would you really like to play? (y/n) ")
